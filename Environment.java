409d64e89b09262132f09297debf9c61268dc65d
reink <omerrn@gmail.com>
2015-12-07 00:34:40 +0200

    Woker env first pass

---
+++ Environment.java
@@ -0,0 +1,10 @@
+package com.cs319.graderpp;
+
+/**
+    This class handles creation and using of docker containers
+ */
+public class Environment {
+
+
+
+}
---
+++ Worker.java
@@ -0,0 +1,11 @@
+package com.cs319.graderpp;
+
+/**
+ * Created by scanozulku on 06.12.2015.
+ */
+public class Worker {
+
+    Submission submission;
+
+
+}
