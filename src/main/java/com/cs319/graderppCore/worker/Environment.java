d579f1f64be857ef1ac770ab30f0b25985ec65f2
ozulkuscan <ozulkuscan@gmail.com>
2015-12-08 01:14:14 +0200

    worker

---
+++ src/main/java/com/cs319/graderppCore/worker/Environment.java
@@ -0,0 +1,16 @@
+package com.cs319.graderppCore.worker
+
+
+public class Environment{
+
+    protected int memoryLimit, timeLimit;
+
+    public Environment(int memoryLimit, int timeLimit) {
+
+    }
+
+    protected runEvaluator (Submission, ){
+        runT = Runtime.getRuntime().exec("docker run amatem/graderpp echo\"daşşak\" ");
+
+    }
+}
---
+++ src/main/java/com/cs319/graderppCore/worker/Main.java
@@ -0,0 +1,9 @@
+package com.cs319.graderppCore.worker
+
+public class Main{
+
+    public static void main(String[] args) {
+        Environment test = new Environment();
+        test.runEvaluator();
+    }
+}
