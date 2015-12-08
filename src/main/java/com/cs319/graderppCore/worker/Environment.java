3a12b85375a9e44d8069e127e6788199e5245f11
ozulkuscan <ozulkuscan@gmail.com>
2015-12-08 02:58:14 +0200

    Docker interaction

--- src/main/java/com/cs319/graderppCore/worker/Environment.java
+++ src/main/java/com/cs319/graderppCore/worker/Environment.java
@@ -9,8 +9,34 @@ public class Environment{
 
     }
 
-    protected runEvaluator (Submission, ){
-        runT = Runtime.getRuntime().exec("docker run amatem/graderpp echo\"daşşak\" ");
+    protected void runEvaluator () throws IOException {
+
+        Runtime rt = Runtime.getRuntime();
+        String[] commands = {"docker","run", "-v", "/home/scanozulku/workspace/cs319/localdropfolder:/dropzone", "-w", "/dropzone", "amatem/graderpp", "sh", "/dropzone/asd.sh"};
+
+        Process proc = rt.exec(commands);
+
+        //docker run --privileged -v /home/workspace/cs319/localdropfolder:/dropzone -w /dropzone amatem/graderpp ./asd.sh
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(proc.getErrorStream()));
+
+
+        // read the output from the command
+        System.out.println("Here is the standard output of the command:\n");
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        // read any errors from the attempted command
+        System.out.println("Here is the standard error of the command (if any):\n");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
 
     }
 }
