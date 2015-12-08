5efd8fdf0441b239fc899daf81968878e575d9b6
ozulkuscan <ozulkuscan@gmail.com>
2015-12-08 05:07:12 +0200

    Docker interaction

--- src/main/java/com/cs319/graderppCore/worker/Environment.java
+++ src/main/java/com/cs319/graderppCore/worker/Environment.java
@@ -1,18 +1,33 @@
-package com.cs319.graderppCore.worker
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Environment{
 
-    protected int memoryLimit, timeLimit;
+    private String stdOutVal ="", stdErrorVal="";
+    private String[] commands = {"docker","run"};
+    // "-v", "/home/scanozulku/workspace/cs319/localdropfolder:/dropzone", "-w", "/dropzone", "amatem/graderpp", "sh", };
 
-    public Environment(int memoryLimit, int timeLimit) {
+    public void addRunParameter(String s){
+        String[] newCommands = new String[commands.length + 1];
+        for(int i=0; i<commands.length; i++){
+            newCommands[i] = commands[i];
+        }
+        newCommands[commands.length] = s;
+        commands = newCommands;
+    }
 
+    public String getStdOutVal() {
+        return stdOutVal;
+    }
+
+    public String getStdErrorVal() {
+        return stdErrorVal;
     }
 
     protected void runEvaluator () throws IOException {
 
         Runtime rt = Runtime.getRuntime();
-        String[] commands = {"docker","run", "-v", "/home/scanozulku/workspace/cs319/localdropfolder:/dropzone", "-w", "/dropzone", "amatem/graderpp", "sh", "/dropzone/asd.sh"};
 
         Process proc = rt.exec(commands);
 
@@ -29,14 +44,15 @@ public class Environment{
         System.out.println("Here is the standard output of the command:\n");
         String s = null;
         while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
+            //System.out.println(s);
+            stdOutVal = stdOutVal + s + "\n";
         }
 
         // read any errors from the attempted command
         System.out.println("Here is the standard error of the command (if any):\n");
         while ((s = stdError.readLine()) != null) {
             System.out.println(s);
+            stdErrorVal = stdErrorVal + s + "\n";
         }
-
     }
-}
+}}
--- src/main/java/com/cs319/graderppCore/worker/Worker.java
+++ src/main/java/com/cs319/graderppCore/worker/Worker.java
@@ -1,5 +1,7 @@
 package com.cs319.graderppCore.worker;
 
+import java.util.UUID;
+import com.cs319.graderppCore.utils;
 public class Worker {
     private String submissionID;
     private String taskID;
@@ -9,7 +11,39 @@ public class Worker {
         this.taskID = taskID;
     }
 
+    public String randomStringGenerate(){
+        String uuid = UUID.randomUUID().toString();
+        return; uuid;
+    }
+
     public void run() {
+        randStr = randomStringGenerate();
+        Environment container = new Environment();
+
+        String compileFolder = Constants.BASE_PATH + "task/" + taskID + "/compile:/tmp/" + randStr + "/task/" + taskID + "/compile";
+        String inputFolder = Constants.BASE_PATH + "task/" + taskID + "/input:/tmp/" + randStr + "/task/" + taskID + "/input";
+        String outputFolder = Constants.BASE_PATH + "task/" + taskID + "/output:/tmp/" + randStr + "/task/" + taskID + "/output";
+        String submissionFile = com.cs319.graderppCore.utils.Constants.BASE_PATH + "submission/" + submissionID + ".cpp:/tmp/" + randStr + "/submission/" + submissionID + ".cpp;
+
+        container.addRunParameter("-v");
+        container.addRunParameter(compileFolder);
+
+        container.addRunParameter("-v");
+        container.addRunParameter(inputFolder);
+
+        container.addRunParameter("-v");
+        container.addRunParameter(outputFolder);
+
+        container.addRunParameter("-v");
+        container.addRunParameter(submissionFile);
+
+
+        container.addRunParameter
+
 
     }
 }
+
+//  /task/TASKId/compile,    /submission/submissionid.cpp
+//  /task/Taskid/input
+//  /task/Taskid/output
